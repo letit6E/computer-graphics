3c28ead48c2472038da67368d60dc89bd738892f
Rustam Shangareev <rustam.letitbe@gmail.com>
2023-11-19 13:58:52 +0300

    Update project structure, implement split method, add tests for split method

--- maze/src/maze/maze.hpp
+++ maze/src/maze/maze.hpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <map>
+#include <set>
 #include <algorithm>
 
 #include "point.hpp"
@@ -11,13 +12,70 @@ enum class MazeError {
 
 class Maze {
 public:
-    Result<bool, MazeError> add_edge(const Point &fst, const Point &snd);
+    Result<bool, MazeError> add_edge(const Point &fst, const Point &snd) {
+        if (fst.x != snd.x && fst.y != snd.y) {
+            return Err(MazeError::NOT_RECTILINEAR);
+        }
+
+        size_t fst_index = add_vertex(fst);
+        size_t snd_index = add_vertex(snd);
+
+        graph[fst_index].push_back(snd_index);
+        graph[snd_index].push_back(fst_index);
+        return Ok(true);
+    }
+
+    void split() {
+        std::vector<int> comps(vertices.size(), -1);
+        int cur_component = 0;
+        for (size_t v = 0; v < vertices.size(); ++v) {
+            if (comps[v] != -1) continue;
+
+            std::vector<size_t> dfs_queue = {v};
+            while (!dfs_queue.empty()) {
+                size_t cur = dfs_queue.back();
+                comps[cur] = cur_component;
+                dfs_queue.pop_back();
+
+                for (const size_t &u: graph[cur]) {
+                    if (comps[u] == -1) {
+                        dfs_queue.push_back(u);
+                    }
+                }
+            }
+
+            ++cur_component;
+        }
+
+        components = std::vector<std::set<Point>>(cur_component);
+        for (size_t v = 0; v < vertices.size(); ++v) {
+            components[comps[v]].insert(vertices[v]);
+        }
+    }
+
+
+    std::vector<std::set<Point>> get_components() {
+        auto result = components;
+        return result;
+    }
 
 private:
-    size_t add_vertex(const Point& v);
+    size_t add_vertex(const Point &v) {
+        const auto it = vertices_indexes.find(v);
+        if (it == vertices_indexes.cend()) {
+            size_t new_index = vertices.size();
+            vertices_indexes[v] = new_index;
+            vertices.push_back(v);
+            graph.emplace_back();
+            return new_index;
+        }
+        return it->second;
+    }
+
 
     std::vector<std::pair<Point, Point>> edges;
     std::vector<Point> vertices;
     std::map<Point, size_t> vertices_indexes;
     std::vector<std::vector<size_t>> graph;
+    std::vector<std::set<Point>> components;
 };
---
+++ maze/tests/maze_tests.cpp
@@ -0,0 +1,42 @@
+
+
+#include <gtest/gtest.h>
+#include "../src/maze/maze.hpp"
+
+TEST(MazeTest, SplitTest) {
+    auto tmp = Maze();
+
+    std::vector<std::vector<double>> input = {
+            {1,   1,  1,  2},
+            {1,   2,  3,  2},
+            {3,   2,  3,  1},
+            {1,   1,  3,  1},
+            {10,  10, 15, 10},
+            {15,  15, 15, 20},
+            {19,  30, 18, 30},
+            {18,  30, 17, 30},
+            {1,   1,  -4, 1},
+            {-10, 1,  -4, 1}
+    };
+    for (const auto &str: input) {
+        double x1 = str[0];
+        double y1 = str[1];
+        double x2 = str[2];
+        double y2 = str[3];
+
+        auto result = tmp.add_edge(Point{x1, y1}, Point{x2, y2});
+        ASSERT_TRUE(result.is_ok());
+    }
+
+    tmp.split();
+    auto comps = tmp.get_components();
+
+    ASSERT_TRUE(
+            (comps == std::vector<std::set<Point>>{
+                    {{-10, 1},  {-4, 1},  {1,  1}, {1, 2}, {3, 1}, {3, 2}},
+                    {{10,  10}, {15, 10}},
+                    {{15,  15}, {15, 20}},
+                    {{17,  30}, {18, 30}, {19, 30}}
+            }));
+}
+
