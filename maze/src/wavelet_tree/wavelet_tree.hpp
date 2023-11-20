e0fa87d17be639b9a9ce81258c2370484c7a812e
Rustam Shangareev <rustam.letitbe@gmail.com>
2023-11-20 22:52:05 +0300

    Implement wavelet tree data structure and lte_count method

--- maze/src/wavelet_tree/wavelet_tree.hpp
+++ maze/src/wavelet_tree/wavelet_tree.hpp
@@ -1,8 +1,54 @@
-
 class WaveletTree {
+private:
+    size_t n;
+    double lo, hi;
+    WaveletTree *left, *right;
+    std::vector<size_t> prefix_cnt;
 public:
-    int get_one() {
-        return 1;
+
+    WaveletTree(std::vector<double> vec) {
+        n = vec.size();
+        if (vec.empty()) {
+            left = nullptr;
+            right = nullptr;
+            return;
+        }
+        lo = *std::min_element(vec.begin(), vec.end());
+        hi = *std::max_element(vec.begin(), vec.end());
+        prefix_cnt.resize(vec.size() + 1);
+
+        if (hi == lo) {
+            prefix_cnt[0] = 0;
+            for (size_t i = 1; i <= vec.size(); ++i) {
+                prefix_cnt[i] = i;
+            }
+            return;
+        }
+
+        double mid = (lo + hi) / 2;
+        for (size_t i = 1; i <= vec.size(); ++i) {
+            prefix_cnt[i] = prefix_cnt[i - 1] + (vec[i - 1] <= mid);
+        }
+
+        std::vector<double> left_vec, right_vec;
+        for (const double& elem : vec) {
+            if (elem <= mid) left_vec.push_back(elem);
+            else right_vec.push_back(elem);
+        }
+
+        left = new WaveletTree(left_vec);
+        right = new WaveletTree(right_vec);
     }
+
+    size_t lte_count(int l, int r, int k) {
+        if (l > r || k < lo) return 0;
+        l = std::max(l, 0);
+        r = std::min(r, static_cast<int>(n) - 1);
+        if (hi <= k) return r - l + 1;
+
+        int left_cnt = static_cast<int>(prefix_cnt[l]), right_cnt = static_cast<int>(prefix_cnt[r + 1]);
+        return left->lte_count(left_cnt, right_cnt - 1, k) + right->lte_count(l - left_cnt, r - right_cnt, k);
+    }
+
 };
 
--- maze/tests/wavelet_tests.cpp
+++ maze/tests/wavelet_tests.cpp
@@ -1,7 +1,19 @@
 #include <gtest/gtest.h>
+#include <iostream>
 #include "../src/wavelet_tree/wavelet_tree.hpp"
 
-TEST(Wavelet_Tests, get_one_test) {
-    auto tmp = WaveletTree();
-    ASSERT_EQ(tmp.get_one(), 1);
+TEST(Wavelet_Tests, lte_count_test) {
+    std::vector<double> x = {3, 1, 2, 4, 0, -1, 4, 1, 10, 4, 2, 1, 10, 39, 12};
+    auto tree = WaveletTree(x);
+
+    ASSERT_EQ(tree.lte_count(0, 4, 2), 3);
+    ASSERT_EQ(tree.lte_count(0, x.size() - 1, 3), 8);
+    ASSERT_EQ(tree.lte_count(3, 2, 1), 0);
+    ASSERT_EQ(tree.lte_count(3, 1000, 1), 4);
+    ASSERT_EQ(tree.lte_count(0, 1, 2), 1);
+    ASSERT_EQ(tree.lte_count(0, 0, 2), 0);
+    ASSERT_EQ(tree.lte_count(1, 1, 2), 1);
+    ASSERT_EQ(tree.lte_count(1, 1, 2), 1);
+    ASSERT_EQ(tree.lte_count(x.size() - 1, x.size() - 1, 13), 1);
+    ASSERT_EQ(tree.lte_count(x.size() - 1, x.size() - 1, 11), 0);
 }
