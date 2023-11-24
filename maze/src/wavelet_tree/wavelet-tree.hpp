4311472037fc1088cde317f70f83e52fe2f83e2a
Rustam Shangareev <rustam.letitbe@gmail.com>
2023-11-24 23:37:23 +0300

    Implement new constructor for wavelet_tree, new method lte_sum, add test_project bash script

--- maze/src/wavelet_tree/wavelet-tree.hpp
+++ maze/src/wavelet_tree/wavelet-tree.hpp
@@ -4,6 +4,7 @@ private:
     double lo, hi;
     WaveletTree *left, *right;
     std::vector<size_t> prefix_cnt;
+    std::vector<size_t> prefix_xor;
 public:
 
     WaveletTree(std::vector<double> vec) {
@@ -18,6 +19,7 @@ public:
         lo = *std::min_element(vec.begin(), vec.end());
         hi = *std::max_element(vec.begin(), vec.end());
         prefix_cnt.resize(vec.size() + 1);
+        prefix_xor = std::vector<size_t>(vec.size() + 1, 0);
 
         if (hi == lo) {
             prefix_cnt[0] = 0;
@@ -36,6 +38,48 @@ public:
         for (const double& elem : vec) {
             if (elem <= mid) left_vec.push_back(elem);
             else right_vec.push_back(elem);
+        }
+
+        left = new WaveletTree(left_vec);
+        right = new WaveletTree(right_vec);
+    }
+
+    WaveletTree(std::vector<std::pair<double, size_t>> vec) {
+        n = vec.size();
+        lo = 1e9;
+        hi = -1e9;
+        if (vec.empty()) {
+            left = nullptr;
+            right = nullptr;
+            return;
+        }
+
+        for (const auto& [x, _] : vec) {
+            lo = std::min(lo, x);
+            hi = std::max(hi, x);
+        }
+        prefix_cnt.resize(vec.size() + 1);
+        prefix_xor.resize(vec.size() + 1);
+        prefix_xor[0] = prefix_cnt[0] = 0;
+
+        if (hi == lo) {
+            for (size_t i = 1; i <= vec.size(); ++i) {
+                prefix_cnt[i] = i;
+                prefix_xor[i] = prefix_xor[i - 1] ^ vec[i - 1].second;
+            }
+            return;
+        }
+
+        double mid = (lo + hi) / 2;
+        for (size_t i = 1; i <= vec.size(); ++i) {
+            prefix_cnt[i] = prefix_cnt[i - 1] + (vec[i - 1].first <= mid);
+            prefix_xor[i] = prefix_xor[i - 1] ^ vec[i - 1].second;
+        }
+
+        std::vector<std::pair<double, size_t>> left_vec, right_vec;
+        for (const auto& [elem, comp] : vec) {
+            if (elem <= mid) left_vec.emplace_back(elem, comp);
+            else right_vec.emplace_back(elem, comp);
         }
 
         left = new WaveletTree(left_vec);
@@ -65,6 +109,16 @@ public:
         } else {
             return right->equal_count(l - left_cnt, r - right_cnt, k);
         }
+    }
+
+    size_t lte_xor(int l, int r, double k) {
+        if (l > r || k < lo) return 0;
+        l = std::max(l, 0);
+        r = std::min(r, static_cast<int>(n) - 1);
+        if (hi <= k) return prefix_xor[r + 1] ^ prefix_xor[l];
+
+        int left_cnt = static_cast<int>(prefix_cnt[l]), right_cnt = static_cast<int>(prefix_cnt[r + 1]);
+        return left->lte_xor(left_cnt, right_cnt - 1, k) ^ right->lte_xor(l - left_cnt, r - right_cnt, k);
     }
 
 };
--- maze/tests/wavelet_tests.cpp
+++ maze/tests/wavelet_tests.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 #include <iostream>
-#include "../src/wavelet_tree/wavelet_tree.hpp"
+#include "../src/wavelet_tree/wavelet-tree.hpp"
 
 TEST(Wavelet_Tests, lte_count_test) {
     std::vector<double> x = {3, 1, 2, 4, 0, -1, 4, 1, 10, 4, 2, 1, 10, 39, 12};
@@ -33,3 +33,93 @@ TEST(Wavelet_Tests, equal_count_test) {
     ASSERT_EQ(tree.equal_count(x.size() - 1, x.size() - 1, 3), 1);
     ASSERT_EQ(tree.equal_count(x.size() - 1, x.size() - 1, 3.1), 0);
 }
+
+TEST(Wavelet_Tests, lte_count_pair_test) {
+    std::vector<std::pair<double, size_t>> x = {{3,  7},
+                                                {1,  3},
+                                                {2,  1},
+                                                {4,  7},
+                                                {0,  9},
+                                                {-1, 415},
+                                                {4,  578},
+                                                {1,  99},
+                                                {10, 1},
+                                                {4,  2},
+                                                {2,  3},
+                                                {1,  4},
+                                                {10, 5},
+                                                {39, 1},
+                                                {12, 2}};
+    auto tree = WaveletTree(x);
+
+    ASSERT_EQ(tree.lte_count(0, 4, 2), 3);
+    ASSERT_EQ(tree.lte_count(0, x.size() - 1, 3), 8);
+    ASSERT_EQ(tree.lte_count(3, 2, 1), 0);
+    ASSERT_EQ(tree.lte_count(3, 1000, 1), 4);
+    ASSERT_EQ(tree.lte_count(0, 1, 2), 1);
+    ASSERT_EQ(tree.lte_count(0, 0, 2), 0);
+    ASSERT_EQ(tree.lte_count(1, 1, 2), 1);
+    ASSERT_EQ(tree.lte_count(1, 1, 2), 1);
+    ASSERT_EQ(tree.lte_count(x.size() - 1, x.size() - 1, 13), 1);
+    ASSERT_EQ(tree.lte_count(x.size() - 1, x.size() - 1, 11), 0);
+}
+
+TEST(Wavelet_Tests, equal_count_pair_test) {
+    std::vector<std::pair<double, size_t>> x = {{3,  7},
+                                                {1,  3},
+                                                {2,  1},
+                                                {4,  7},
+                                                {2,  9},
+                                                {-1, 415},
+                                                {4,  578},
+                                                {1,  99},
+                                                {10, 1},
+                                                {3,  2},
+                                                {2,  3},
+                                                {1,  4},
+                                                {10, 5},
+                                                {39, 1},
+                                                {3, 2}};
+    auto tree = WaveletTree(x);
+
+    ASSERT_EQ(tree.equal_count(0, 4, 2), 2);
+    ASSERT_EQ(tree.equal_count(0, x.size() - 1, 3), 3);
+    ASSERT_EQ(tree.equal_count(3, 2, 1), 0);
+    ASSERT_EQ(tree.equal_count(3, 1000, 10), 2);
+    ASSERT_EQ(tree.equal_count(0, 1, 1), 1);
+    ASSERT_EQ(tree.equal_count(0, 0, 3), 1);
+    ASSERT_EQ(tree.equal_count(1, 1, 3), 0);
+    ASSERT_EQ(tree.equal_count(1, 1, 1), 1);
+    ASSERT_EQ(tree.equal_count(x.size() - 1, x.size() - 1, 3), 1);
+    ASSERT_EQ(tree.equal_count(x.size() - 1, x.size() - 1, 3.1), 0);
+}
+
+TEST(Wavelet_Tests, lte_sum_pair_test) {
+    std::vector<std::pair<double, size_t>> x = {{3,  7},
+                                                {1,  3},
+                                                {2,  1},
+                                                {4,  7},
+                                                {0,  9},
+                                                {-1, 415},
+                                                {4,  578},
+                                                {1,  99},
+                                                {10, 1},
+                                                {4,  2},
+                                                {2,  3},
+                                                {1,  4},
+                                                {10, 5},
+                                                {39, 1},
+                                                {12, 2}};
+    auto tree = WaveletTree(x);
+
+    ASSERT_EQ(tree.lte_xor(0, 4, 2), 3 ^ 1 ^ 9);
+    ASSERT_EQ(tree.lte_xor(0, x.size() - 1, 3), 7 ^ 3 ^ 1 ^ 9 ^ 415 ^ 99 ^ 3 ^ 4);
+    ASSERT_EQ(tree.lte_xor(3, 2, 1), 0);
+    ASSERT_EQ(tree.lte_xor(3, 1000, 1), 415 ^ 9 ^ 99 ^ 4);
+    ASSERT_EQ(tree.lte_xor(0, 1, 2), 3);
+    ASSERT_EQ(tree.lte_xor(0, 0, 2), 0);
+    ASSERT_EQ(tree.lte_xor(1, 1, 2), 3);
+    ASSERT_EQ(tree.lte_xor(x.size() - 1, x.size() - 1, 13), 2);
+    ASSERT_EQ(tree.lte_xor(x.size() - 1, x.size() - 1, 11), 0);
+}
+
